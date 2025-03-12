91312325cc806ec5a214cef507d714f7b6584d9a
Satyam Sah <satnarayan5166@gmail.com>
2025-03-12 17:28:46 +0545

    2's table

--- 03-basic-knowledge-excersise/script.js
+++ 03-basic-knowledge-excersise/script.js
@@ -1,9 +1,20 @@
-let marks = 60;
+// let marks = 60;
 
-if(marks > 60){
-    console.log('You are pass');
-} else{
-    console.log('You are fail')
+// if(marks > 60){
+//     console.log('You are pass');
+// } else{
+//     console.log('You are fail')
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(2**1);
+    console.log(2**2);
+    console.log(2**3-2);
+    console.log(2**3);
+    console.log(2**4-6);
+    console.log(2**4-4);
+    console.log(2**4-2);
+    console.log(2**4);
+    console.log(2**4+2);
+    console.log(2**4+4);
 }
-
-
