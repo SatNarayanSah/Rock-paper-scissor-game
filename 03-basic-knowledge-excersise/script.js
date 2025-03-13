6537d5417df8945082973821036a2bb65ee7d0e1
Satyam Sah <satnarayan5166@gmail.com>
2025-03-13 20:25:36 +0545

    Print
    1,2,3,4,5,6,7,8,9,10
    11,12,13,14,15,16,17,18,19,20
    .
    .
    ......................99,100 solved

--- 03-basic-knowledge-excersise/script.js
+++ 03-basic-knowledge-excersise/script.js
@@ -6,15 +6,52 @@
 //     console.log('You are fail')
 // }
 
-for (let i = 1; i <= 10; i++) {
-    console.log(2**1);
-    console.log(2**2);
-    console.log(2**3-2);
-    console.log(2**3);
-    console.log(2**4-6);
-    console.log(2**4-4);
-    console.log(2**4-2);
-    console.log(2**4);
-    console.log(2**4+2);
-    console.log(2**4+4);
+// for (let i = 1; i <= 10; i++) {
+//     console.log(2**1);
+//     console.log(2**2);
+//     console.log(2**3-2);
+//     console.log(2**3);
+//     console.log(2**4-6);
+//     console.log(2**4-4);
+//     console.log(2**4-2);
+//     console.log(2**4);
+//     console.log(2**4+2);
+//     console.log(2**4+4);
+// }
+
+// take tow number and add a value that is the sum of te two numbers plus another number. Now find the 3r number. Write the logic
+
+// let a = 5;
+// let b=6;
+// sum = a +b + 7;
+// console.log(sum);
+// console.log(sum - (a+b));
+
+// for(let i=1; i<=100; i++){
+//     if(i%5 == 0 && i%15 == 0){
+//         console.log(i);
+//     }
+// }
+
+// calculat sum of all numbers
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// for (let i = 0; i < 12; i++) {
+//   console.log(2 ** i);
+// }
+
+let result = '';
+for (let i = 1; i <= 100; i++) {
+  result += i;
+  if(i%10 === 0){
+    result += '\n'
+  } else{
+    result += ','
+  }
 }
+console.log(result)
