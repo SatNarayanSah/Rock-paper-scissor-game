1781fc8edc0ee220b0381d8c2e06ee624fee7ef2
Satyam Sah <satnarayan5166@gmail.com>
2025-03-15 19:25:56 +0545

    splt the sentence using space and find the length

---
+++ string-excersise/script.js
@@ -0,0 +1,5 @@
+// const str = "My name is learn javascript"; split this stirng using space and find length
+const str = "My name is learn javascript";
+// console.log(str.length)
+const words = str.split(' ');
+console.log(words.length)
