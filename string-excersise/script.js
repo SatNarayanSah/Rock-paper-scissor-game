db1414db6423d8f59c337293c3442fdafaac9328
Satyam Sah <satnarayan5166@gmail.com>
2025-03-19 16:05:58 +0545

    write the program to check is a tring is palindrome or not solved

--- string-excersise/script.js
+++ string-excersise/script.js
@@ -1,5 +1,19 @@
 // const str = "My name is learn javascript"; split this stirng using space and find length
-const str = "My name is learn javascript";
-// console.log(str.length)
-const words = str.split(' ');
-console.log(words.length)
+// const str = "My name is learn javascript";
+// // console.log(str.length)
+// const words = str.split(' ');
+// console.log(words.length)
+
+
+
+// write the program to check is a tring is palindrome or not?
+let str='maam';
+let str2 = '';
+for(let i=str.length-1; i>=0; i--){
+    str2 += str[i];
+}
+if(str === str2){
+    console.log('palindrome');
+}else{
+    console.log("Not palindrom")
+}
