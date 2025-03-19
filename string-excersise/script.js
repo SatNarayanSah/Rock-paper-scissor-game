80bbed4f0eaa347b4fb3c483c25f9c0aca25178f
Satyam Sah <satnarayan5166@gmail.com>
2025-03-19 16:25:25 +0545

    slice with padEnd and pasStart function working method solved

--- string-excersise/script.js
+++ string-excersise/script.js
@@ -7,13 +7,28 @@
 
 
 // write the program to check is a tring is palindrome or not?
-let str='maam';
-let str2 = '';
-for(let i=str.length-1; i>=0; i--){
-    str2 += str[i];
-}
-if(str === str2){
-    console.log('palindrome');
-}else{
-    console.log("Not palindrom")
-}
+// let str='maam';
+// let str2 = '';
+// for(let i=str.length-1; i>=0; i--){
+//     str2 += str[i];
+// }
+// if(str === str2){
+//     console.log('palindrome');
+// }else{
+//     console.log("Not palindrom")
+// }
+
+
+
+// 9581894461 Convert to below format
+/* 95818*****
+*****94461
+958**944** */
+
+let str = '9581894461';
+let str1 = str.slice(0,5).padEnd(10,'*');
+let str2 = str.slice(5,10).padStart(10,'*');
+const str3 = str.slice(0, 3) + "**" + str.slice(5, 8) + "**";
+console.log(str1);
+console.log(str2);
+console.log(str3);
